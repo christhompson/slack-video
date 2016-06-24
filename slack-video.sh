1ec6f3aaa3168fadb05efb6507211909adf9e465
Chris Thompson <chris.j.thompson@gmail.com>
2016-06-23 21:36:04 -0700

    Convert to showing frames as attachment.
    Switch from showing the frame as the message text and instead
    display as an attachment. This gets around the 4K limit, and
    is a little cleaner (we can show context around it, etc.).
    Downside is you have to expand the attachment to watch (this
    could also be seen as a benefit).

--- slack-video.sh
+++ slack-video.sh
@@ -1,52 +1,88 @@
 #!/bin/bash
 
-######################
-# CONFIGURATION
-######################
-# Run in channel #video-player
-CHANNEL="C1KSMEQ76"
-MESSAGE="Let's watch a video!"
-TITLE="ETI SlackBot Video"
+#################
+# CONFIGURATION #
+#################
+CHANNEL="C1KSMEQ76"  # Run in channel #video-player
+MESSAGE="Let's watch a video! Expand attachment to watch!"
+ENDMESSAGE="Thanks for watching!"
+FPS=1
+WIDTH=70
 APPDIR=~/.slack-video/
 LOCK=$APPDIR/lock
-FPS=1
-WIDTH=60
 
 function videoToJpgs {
     # Convert video to images using FFMPEG
-    ffmpeg -i $1 -vf fps=$FPS $2/thumb%04d.jpg >/dev/null 2>/dev/null
+    # $1 is path of video file to convert
+    ffmpeg -i $1 -vf fps=$FPS $2/thumb%10d.jpg >/dev/null 2>/dev/null
 }
 
 function newMsg {
-    # $1 is message
+    # $1 is videoname
+    # Creates a new slack message and returns the timestamp.
     curl https://slack.com/api/chat.postMessage -X POST \
     -d "token=$TOKEN" \
     -d "channel=$CHANNEL" \
-    -d "text=\`\`\`$MESSAGE\`\`\`" \
+    -d "text=$MESSAGE" \
+    --data-urlencode "attachments=
+    [
+        {
+             \"pretext\": \"Video will start in 10s...\",
+             \"title\": \"$1\",
+             \"text\": \"\`\`\`Expand attachment to watch\\n\\n\\n\\n\\n\`\`\`\",
+             \"mrkdwn_in\": [
+                 \"text\"
+             ]
+         }
+     ]" \
     -d "pretty=1" 2> /dev/null | jq '.ts'
 }
 
-function updateMsg {
-    # $1 is timestamp, $2 is message
+function showFrame {
+    # $1 is videoname, $2 is timestamp, $3 is frame
+    # Displays frame as an attachment to the message.
+    curl https://slack.com/api/chat.update -X POST \
+    -d "token=$TOKEN" \
+    -d "ts=$2" \
+    -d "channel=$CHANNEL" \
+    -d "text=$MESSAGE" \
+    --data-urlencode "attachments=
+    [
+        {
+             \"pretext\": \"Video is playing...\",
+             \"title\": \"$1\",
+             \"text\": \"\`\`\`$3\`\`\`\",
+             \"mrkdwn_in\": [
+                 \"text\"
+             ]
+         }
+     ]" \
+    -d "pretty=1" 1> /dev/null 2> /dev/null
+}
+
+function endVideo {
+    # $1 is timestamp
+    # Thanks the viewer, removes attachment.
     curl https://slack.com/api/chat.update -X POST \
     -d "token=$TOKEN" \
     -d "ts=$1" \
     -d "channel=$CHANNEL" \
-    -d "text=\`\`\`$2\`\`\`" \
+    -d "text=$ENDMESSAGE" \
+    --data-urlencode "attachments=[]" \
     -d "pretty=1" 1> /dev/null 2> /dev/null
 }
 
-###########
-# MAIN
-###########
+########
+# MAIN #
+########
 
 if [ -z "$TOKEN" ]; then
-    echo "Must set TOKEN environment variable with slack API token."
+    echo "Must set TOKEN environment variable with slack API token"
     exit 1
 fi
 
 if [ ! $# -eq 1 ]; then
-    echo "Must provide video file as only argument"
+    echo "Must provide video file as only argument."
     exit 1
 fi
 
@@ -55,9 +91,9 @@ mkdir -p $APPDIR
 # mkdir is be atomic on *nix, so we use it for a mutex.
 # See http://wiki.bash-hackers.org/howto/mutex
 if mkdir $LOCK; then
-  echo "Locking succeeded" >&2
+  echo "Locking succeeded."
 else
-  echo "Video player already running. Only one video at a time allowed." >&2
+  echo "Video player already running. Only one video at a time allowed."
   exit 1
 fi
 
@@ -74,7 +110,7 @@ if [ ! -e "$APPDIR/thumbs/$videoname/" ]; then
 fi
 
 echo "Creating new slack message player in #video-player. Playback starts in 10s."
-timestamp=$(newMsg $MESSAGE)
+timestamp=$(newMsg $videoname)
 
 sleep 10
 
@@ -82,13 +118,12 @@ FILES="$APPDIR/thumbs/$videoname/*"
 for f in $FILES
 do
   echo "Displaying file $f..."
-  updateMsg $timestamp "$(jp2a --width=$WIDTH $f)"
-#   sleep 1
+  showFrame $videoname $timestamp "$(jp2a --width=$WIDTH $f)"
 done
 
-updateMsg $timestamp "[End of video]"
+endVideo $timestamp
 
-# Release the lock
+# Release the lock.
 rmdir $LOCK
 
 echo "Done playing. Lock released."
